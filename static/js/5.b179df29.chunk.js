ba18563d6f3f0307d8cab57e4735c2c763938c75
vimal kovath <vimal.k@fulfillsolutions.com>
2019-12-08 10:48:55 +0530

    Updates

---
+++ static/js/5.b179df29.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgrabtest=this.webpackJsonpgrabtest||[]).push([[5],{227:function(t,n,e){"use strict";e.r(n),e.d(n,"startStatusTap",(function(){return i}));var o=e(31),i=(e(9),function(){var t=window;t.addEventListener("statusTap",(function(){Object(o.g)((function(){var n=t.innerWidth,e=t.innerHeight,i=document.elementFromPoint(n/2,e/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(o.m)((function(){return r.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.b179df29.chunk.js.map
